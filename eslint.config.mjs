6ea58c5304621a33c0ed8b0cd4c41a33756230dd
abdelrahmanHamdyG <dhamdy078@gmail.com>
2024-09-06 05:43:59 -0400

    setting the ES-lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,27 @@
 import globals from "globals";
+import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["src/**/*.{js,mjs,cjs,ts}"],
+
+    rules: {
+      'quotes': ['error', 'double'],
+      'semi': ['error', 'always'],
+      'max-len': ['error', { code: 80 }],
+      'no-unused-vars': ['error'],
+      '@typescript-eslint/explicit-function-return-type': 'error',  
+    },
+  },
+  {
+    files: ["src/**/*.js"], languageOptions: {sourceType: "commonjs"}
+  },
+  {  
+    languageOptions: { globals: {...globals.browser, ...globals.node} }
+  },
+
+  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
 ];
