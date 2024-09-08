b2d7b3b21416f162cbf13753bc668f4d823b2f37
Ahmed Shebl <ahmadshebl8096@gmail.com>
2024-09-08 11:09:55 -0400

    Initial npm api data fetch implementaition

--- src/API_fetch.ts
+++ src/API_fetch.ts
@@ -1,34 +1,51 @@
-import { Octokit } from "octokit";
-import dotenv from "dotenv";
+//import { Octokit } from "octokit";
+// import dotenv from "dotenv";
+// dotenv.config();
 
+//const env: NodeJS.ProcessEnv = process.env;
 
-dotenv.config();
+// export async function fetchRepoData(
+//     owner: string,
+//     name: string,
 
-const env: NodeJS.ProcessEnv = process.env;
+// ): Promise <void | JSON> {
 
-export async function fetchRepoData(
-    owner: string,
-    name: string,
-
-): Promise <void | JSON> {
-    try {
+//     try {
        
-        const octokit = new Octokit({
-            auth: env.GITHUB_TOKEN
-        });
+//         const octokit = new Octokit({
+//             auth: env.GITHUB_TOKEN
+//         });
         
-      const response =  await octokit.request("GET /repos/{owner}/{repo}", {
-        owner: owner,
-        repo: name,
-        headers: {
-          "X-GitHub-Api-Version": "2022-11-28"
-        }
-      });
-      return response.data as JSON;
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
+//       const response =  await octokit.request("GET /repos/{owner}/{repo}", {
+//         owner: owner,
+//         repo: name,
+//         headers: {
+//           "X-GitHub-Api-Version": "2022-11-28"
+//         }
+//       });
+//       return response.data as JSON;
+//     } catch (error) {
+//       console.error("Error fetching data:", error);
+//     }
+//   };
 
+  export async function fetchNpmPackageData
+  (packageName : string): Promise <void | JSON> {
+    const url = `https://api.npms.io/v2/package/${packageName}`;
+
+  try {
+    console.log("we are here");
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log("this is the data returned from the npm \n"+data);
+    const repository = data?.collected?.metadata?.repository;
+    console.log(repository);
+    
+    return data;
+
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+  };
 
 
--- src/CLI.ts
+++ src/CLI.ts
@@ -1,5 +1,5 @@
-import { fetchRepoData } from "./API_fetch.js";
-import { PackageData } from "./PackageData.js";
+import { fetchNpmPackageData } from "./API_fetch.js";
+//import { PackageData } from "./PackageData.js";
 import { exec } from "child_process";
 
 export class CLI {
@@ -11,18 +11,30 @@ export class CLI {
     
     
     public rankModules(path:string):void {
-
-        fetchRepoData("abdelrahmanHamdyG", "ECE-46100-Project-").then(
+        console.log("we are in the rank modules");
+        fetchNpmPackageData("even").then(
             (res:void | JSON) =>{
-                if (res){
-                const res_obj= res as PackageData ;
+                // if (res){
+                // const res_obj= res as PackageData ;
                 
-                const res_info: PackageData = {
-                    name: res_obj.name,
-                };
-                console.log(res_info.name);
-            }
+                // const res_info: PackageData = {
+                //     name: res_obj.name,
+                // };
+                 console.log(res);
+            
             });
+
+        // fetchRepoData("abdelrahmanHamdyG", "ECE-46100-Project-").then(
+        //     (res:void | JSON) =>{
+        //         if (res){
+        //         const res_obj= res as PackageData ;
+                
+        //         const res_info: PackageData = {
+        //             name: res_obj.name,
+        //         };
+        //         console.log(res_info.name);
+        //     }
+        //     });
         console.log("we found the file " + path);
 
     }
