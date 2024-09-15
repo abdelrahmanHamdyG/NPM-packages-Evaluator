034386b96ebee685439f1b41d6459df923107f06
Zeyad7007 <zeyadel3fifi@gmail.com>
2024-09-15 19:10:22 -0400

    data fetching and metrics handled

--- src/GitHubAPI.ts
+++ src/GitHubAPI.ts
@@ -3,10 +3,9 @@ import { Octokit } from "octokit";
 import { GitHubData } from "./GitHubData.js";
 import { API } from "./API.js";
 import { Issue } from "./IssueInterface.js";
-import { url } from "inspector";
+import { Contributor } from "./ContributorInterface.js";
 
-
-export class GitHubAPI extends API{
+export class GitHubAPI extends API {
     private owner: string;
     private repoName: string;
 
@@ -16,19 +15,18 @@ export class GitHubAPI extends API{
         this.repoName = repoName;
     }
 
-    public async fetchData():  Promise<GitHubData>  {
+    public async fetchData(): Promise<GitHubData> {
         const octokit = new Octokit({
             auth: process.env.GITHUB_TOKEN
         });
 
         try {
-            
             this.logger.log(2, 
                 `Fetching data for owner: ${this.owner}, repo: 
                 ${this.repoName}`);
 
-            const reposResponse = await octokit
-            .request("GET /repos/{owner}/{repo}", {
+            // Initialize API requests
+            const reposRequest = octokit.request("GET /repos/{owner}/{repo}", {
                 owner: this.owner,
                 repo: this.repoName,
                 headers: {
@@ -36,92 +34,149 @@ export class GitHubAPI extends API{
                 }
             });
 
-            const issues: Issue[] = [];
-            let page = 1;
-            let perPage = 100;
-            let moreIssues = true;
-
-            const currentDate = new Date();
-
-            const threeMonthsAgo = new Date(currentDate.setMonth(currentDate.getMonth() - 3)).toISOString();
-
-            while (moreIssues) {
-                const issuesResponse = await octokit.request("GET /repos/{owner}/{repo}/issues", {
-                    owner: this.owner,
-                    repo: this.repoName,
-                    headers: {
-                        "X-GitHub-Api-Version": "2022-11-28"
-                    },
-                    page: page,
-                    per_page: perPage,
-                    state: "closed",
-                    since: threeMonthsAgo 
-                });
-
-                const fetchedIssues = issuesResponse.data as Issue[];
-
-                if (fetchedIssues.length === 0) {
-                    moreIssues = false;
-                } else {
-                    issues.push(...fetchedIssues);
-                    page++;
+            const commitsRequest = octokit.request("GET /repos/{owner}/{repo}/commits", {
+                owner: this.owner,
+                repo: this.repoName,
+                headers: {
+                    "X-GitHub-Api-Version": "2022-11-28"
                 }
+            });
+
+            // Fetch issues and contributors with pagination
+            const issuesRequest = this.fetchIssues(octokit);
+            const contributorsRequest = this.fetchContributors(octokit);
+
+            const readmeRequest = octokit.request("GET /repos/{owner}/{repo}/readme", {
+                owner: this.owner,
+                repo: this.repoName,
+                headers: {
+                    "X-GitHub-Api-Version": "2022-11-28"
+                }
+            });
+
+            // Wait for all data fetches to complete in parallel
+            const [reposResponse, issues, commitsResponse, contributors, readmeResponse] = await Promise.all([
+                reposRequest,
+                issuesRequest,
+                commitsRequest,
+                contributorsRequest,
+                readmeRequest.catch(() => null)
+            ]);
+
+            // Process contributors data
+            const totalContributions = (contributors as Contributor[]).map((contributor: Contributor) => ({
+                contributor: contributor.author.login,
+                totalLinesAdded: contributor.total,
+                commits: contributor.contributions
+            }));
+
+            const totalCodeLines = totalContributions.reduce((sum, contributor) => {
+                return sum + contributor.totalLinesAdded;
+            }, 0);
+
+            // Check if the repo has a README and description
+            const readmeFound = readmeResponse ? true : false;
+            const descriptionFound = reposResponse.data.description ? true : false;
+            let license = "empty";
+            if (reposResponse.data.license != null) {
+                license = reposResponse.data.license.name;
             }
 
-
-
-            const commitsResponse = await octokit
-            .request("GET /repos/{owner}/{repo}/commits", {
-                owner: this.owner,
-                repo: this.repoName,
-                headers: {
-                    "X-GitHub-Api-Version": "2022-11-28"
-                }
+            // Calculate the time to close issues
+            const timesToClose: number[] = issues.map((issue: Issue) => {
+                const createdAt = new Date(issue.created_at);
+                const closedAt = new Date(issue.closed_at);
+                return (closedAt.getTime() - createdAt.getTime()) / (1000 * 60 * 60 * 24); // Difference in days
             });
 
-            const contributors = await octokit
-            .request("GET /repos/{owner}/{repo}/contributors", {
-                owner: this.owner,
-                repo: this.repoName,
-                headers: {
-                    "X-GitHub-Api-Version": "2022-11-28"
-                }
-            });
-
-            const contributionsArray: number[] = [];
-            for (let i = 0; i < contributors.length; i++) {
-                contributionsArray.push(contributors[i].contributions);
-              }
-
-            const readmeFound = 
-            reposResponse.data.readme ? true : false;
-            const descriptionFound = 
-            reposResponse.data.description ? true : false;
-            let license="empty";
-            if(reposResponse.data.license!=null)
-                license= reposResponse.data.license.name
-            
-// 
-            
-            
-            
-            if(this.repoName=="express"){
-                
-                
-                 console.log(issues[0]);
-            }
-                
-            return new GitHubData(reposResponse.data.name,
-                 issues.length, commitsResponse.data.length,
-                 contributionsArray,readmeFound,descriptionFound,
-                 reposResponse.data.forks_count,
-                 reposResponse.data.stargazers_count,
-                 license,
-                 issues);
+            this.logger.log(2, "Successfully fetched data from GitHub API");
+            return new GitHubData(
+                reposResponse.data.name,
+                issues.length,
+                commitsResponse.data.length,
+                reposResponse.data.forks_count,
+                reposResponse.data.stargazers_count,
+                reposResponse.data.collaborators_count,
+                readmeFound,
+                descriptionFound,
+                totalContributions,
+                totalCodeLines,
+                timesToClose,
+                license,
+                issues
+            );
         } catch (error) {
-            if(error)
+            if (error) {
                 this.logger.log(2, `Error fetching data: ${error} for the repo ${this.repoName}`);
+            }
+            return new GitHubData(); // Return empty data on error
         }
-        return new GitHubData();
+    }
+
+    // Fetch issues with pagination
+    private async fetchIssues(octokit: Octokit): Promise<Issue[]> {
+        const issues: Issue[] = [];
+        let page = 1;
+        const perPage = 100;
+        let moreIssues = true;
+
+        const currentDate = new Date();
+        const threeMonthsAgo = new Date();
+        threeMonthsAgo.setMonth(currentDate.getMonth() - 3);
+
+        while (moreIssues) {
+            const issuesResponse = await octokit.request("GET /repos/{owner}/{repo}/issues", {
+                owner: this.owner,
+                repo: this.repoName,
+                headers: {
+                    "X-GitHub-Api-Version": "2022-11-28"
+                },
+                page: page,
+                per_page: perPage,
+                state: "closed",
+                since: threeMonthsAgo.toISOString()
+            });
+
+            const fetchedIssues = issuesResponse.data as Issue[];
+            if (fetchedIssues.length === 0) {
+                moreIssues = false;
+            } else {
+                issues.push(...fetchedIssues);
+                page++;
+            }
+        }
+
+        return issues;
+    }
+
+    // Fetch contributors with pagination
+    private async fetchContributors(octokit: Octokit): Promise<Contributor[]> {
+        const contributors: Contributor[] = [];
+        let page = 1;
+        const perPage = 100;
+        let moreContributors = true;
+
+        while (page<5) {
+            const contributorsResponse = await octokit.request("GET /repos/{owner}/{repo}/stats/contributors", {
+                owner: this.owner,
+                repo: this.repoName,
+                headers: {
+                    "X-GitHub-Api-Version": "2022-11-28"
+                },
+                page: page,
+                per_page: perPage
+            });
+
+            const fetchedContributors = contributorsResponse.data as Contributor[];
+
+            if (fetchedContributors.length === 0) {
+                moreContributors = false;
+            } else {
+                contributors.push(...fetchedContributors);
+                page++;
+            }
+        }
+
+        return contributors;
     }
 }
