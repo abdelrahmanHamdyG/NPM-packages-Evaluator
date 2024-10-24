42f0e7988c949a7bde4961b98e537cae331f59b2
Parthiv Shaji <parthiv6@gmail.com>
2024-10-24 11:18:35 -0400

    Added S3 functionality

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,9 +3,20 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 // import packagesRouter from './routes/packages';
 // import metricsRouter from './routes/metrics';
+import { uploadFile, downloadFile } from './services/s3Service.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const bucketName = 'ece461storage';
+const key = 'test-file.txt';
+// Upload a file
+uploadFile(bucketName, key, 'Hello, AWS S3!').then(() => {
+    // Download the same file after uploading
+    downloadFile(bucketName, key).then((data) => {
+        console.log('Downloaded content:', data);
+    });
+});
 
 // Middleware
 app.use(cors());
